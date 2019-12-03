f4bcac37cbc703b20ccf09214fb632755f82ca55
Tommy Berman <tomy.berman1@gmail.com>
2019-12-03 22:18:58 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -47,11 +47,8 @@ def total_even_pairs(src)
     even = []
     total = even.sum
   src.length.times do |counter|
-    if (src[counter][0] %2 == 0)
-      even.push(src[counter][0])
-    end
-    if (src[counter][1] %2 == 0)
-      even.push(src[counter][1])
+    if (src[counter] %2 == 0)
+      even.push(src[counter])
     end
   end
   total
